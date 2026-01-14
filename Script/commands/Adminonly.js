5db4af4031f8215265ce4476a0fbd7737020a821
Rahat Islam <rahatbott10@gmail.com>
2026-01-14 22:04:17 +0600

    Update Adminonly.js

--- Script/commands/Adminonly.js
+++ Script/commands/Adminonly.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "onlyadmin",
  version: "1.0",
  hasPermssion: 2,
- credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+ credits: "🔰𝐑𝐀𝐇𝐀𝐓 𝐈𝐒𝐋𝐀𝐌🔰",
  description: "Admin only",
  commandCategory: "Admin",
  usages: "qtvonly",
