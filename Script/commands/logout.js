17d8a994f568fbec156a98dd0fac0d2299b98752
Rahat Islam <rahatbott10@gmail.com>
2026-01-17 17:31:55 +0600

    Update logout.js

--- Script/commands/logout.js
+++ Script/commands/logout.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "logout",
     version: "1.0.1",
     hasPermssion: 2,
-    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+    credits: "🔰𝐑𝐀𝐇𝐀𝐓 𝐈𝐒𝐋𝐀𝐌🔰",
     description: "Logout ACC Bot",
     commandCategory: "System",
     usages: "",
